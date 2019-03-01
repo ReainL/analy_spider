084fd7bc7769158f27906f8854895ed3d9d6e907
xusl <xusl@chyjr.com>
2019-03-01 13:33:07 +0800

    修改sh脚本路径

--- analy_spider/task_baidu_craw.sh
+++ analy_spider/task_baidu_craw.sh
@@ -2,10 +2,9 @@
 
 #MORNINGDATA_HOME=$(cd `dirname $0`; pwd)
 #
-#python3 ${MORNINGDATA_HOME}/news_cj.py
-#python3 ${MORNINGDATA_HOME}/index_real_time.py
+#python3 ${MORNINGDATA_HOME}/baidu_crawler.py
 
 #MORNINGDATA_HOME=$(cd `dirname $0`; pwd)
-#cd /root/morningdata/morningData/morningdata
-python3 /root/morningdata/morningData/morningdata/baidu_crawler.py
+#cd /home/xsl/reainl/reainl/analy_spider/analy_spider
+python3 /home/xsl/reainl/reainl/analy_spider/analy_spider/baidu_crawler.py
 
